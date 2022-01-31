d38e82d800f7fd53269ab524b340d37187d56bde
Анна Валитова <anavalitova0@gmail.com>
2022-01-31 19:42:35 +1000

    Добавьте файлы проекта.

---
+++ Список вы знаете кого)/Hum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Список_вы_знаете_кого_
+{
+    public class Hum
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Year  { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
---
+++ Список вы знаете кого)/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Список_вы_знаете_кого_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Hum> DataArray { get; set; }
+        public Hum SelectedHum { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            DataArray = new ObservableCollection<Hum>();
+            DataArray.Add(new Hum { Name = "Степанида", Species = "Курильский Бобтейл", Year = 2017, ImagePath = "/Коты/бобтейл.jpg" });
+            DataArray.Add(new Hum { Name = "Лари", Species = "Британская-короткошёрстная", Year = 2019, ImagePath = "/Коты/Британская-короткошерстная.jpg" });
+            DataArray.Add(new Hum { Name = "Рафик", Species = "Манчкин", Year = 2020, ImagePath = "/Коты/manchkiny.jpg" });
+        }
+    }
+}
